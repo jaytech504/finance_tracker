d9548a745ca2d118da96498d60ec9002fc80094e
jaytech524 <jasonyakubu543@gmail.com>
2024-12-11 11:52:29 +0100

    reports page

--- fin_track/urls.py
+++ fin_track/urls.py
@@ -3,8 +3,11 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('transaction/', views.add_transaction, name='transact'),
     path('summary/', views.summary, name='summary'),
+    path('report/', views.report, name='report'),
     path('fetch-transactions/', views.fetch_transactions, name='fetch_transactions'),
+    path('download-transaction/', views.download_transactions, name='download_transactions'),
+    path('download-csv/', views.download_transactions_csv, name='download_transactions_csv'),
+    path('download-pdf/', views.download_transactions_pdf, name='download_transactions_pdf'),
 
 ]
--- fin_track/views.py
+++ fin_track/views.py
@@ -1,12 +1,17 @@
 from django.shortcuts import render, redirect
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpResponse
 from .models import Transaction, LinkedAccount, BankTransaction
 from .forms import TransactionForm
 from django.views.decorators.csrf import csrf_exempt
 from django.contrib.auth.decorators import login_required
 from .utils import save_transactions
+from reportlab.pdfgen import canvas
+from reportlab.lib.pagesizes import letter
+from reportlab.platypus import SimpleDocTemplate, Table, TableStyle
+from reportlab.lib import colors
 import json
 import os
+import csv
 from django.db.models import Sum
 from decimal import Decimal
 from io import BytesIO
@@ -17,11 +22,20 @@ from datetime import datetime, timedelta
 
 # Create your views here.
 def home(request):
+    if request.method == 'POST':
+        form = TransactionForm(request.POST)
+        
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    else:
+        form = TransactionForm()
+
     total_income = Transaction.total_income()
     total_expense = Transaction.total_expense()
     balance = Transaction.balance()
     tot_history = list(Transaction.objects.all())
-    trans_history = tot_history[-1:-6:-1]
+    trans_history = tot_history[-1:-11:-1]
 
     transactions = Transaction.objects.all()
 
@@ -65,6 +79,7 @@ def home(request):
             'expense_values': expense_values,
             "accounts": accounts,
             "transactions_data": transactions_data,
+            'form': form,
         }
     return render(request, 'fin_track/index.html', context)
     
@@ -81,21 +96,73 @@ def summary(request):
     }
     return render(request, 'fin_track/summary.html', context)
 
-def add_transaction(request):
-    if request.method == 'POST':
-        form = TransactionForm(request.POST)
-        
-        if form.is_valid():
-            form.save()
-            return redirect('home')
+def report(request):
+    return render(request, 'fin_track/report.html')
+
+import csv
+
+def download_transactions_csv(request):
+    start_date = request.GET.get('start_date')
+    end_date = request.GET.get('end_date')
+
+    if start_date and end_date:
+        transactions = Transaction.objects.filter(
+            date__range=[start_date, end_date]
+        )
     else:
-        form = TransactionForm()
+        transactions = Transaction.objects.all()
 
-    context = {
-        'form': form,
-    }
+    response = HttpResponse(content_type='text/csv')
+    response['Content-Disposition'] = 'attachment; filename="transactions.csv"'
 
-    return render(request, 'fin_track/transaction.html', context)
+    writer = csv.writer(response)
+    writer.writerow(["Date", "Description", "Amount", "Type"])
+
+    for transaction in transactions:
+        writer.writerow([transaction.date, transaction.description, transaction.amount, transaction.type])
+
+    return response
+
+def download_transactions_pdf(request):
+    start_date = request.GET.get('start_date')
+    end_date = request.GET.get('end_date')
+
+    if start_date and end_date:
+        transactions = Transaction.objects.filter(
+            date__range=[start_date, end_date]
+        )
+    else:
+        transactions = Transaction.objects.all()
+
+    # Prepare data for table
+    data = [["Date", "Description", "Amount", "Type"]]
+    for transaction in transactions:
+        data.append([transaction.date, transaction.description, transaction.amount, transaction.type])
+
+    # Generate PDF
+    response = HttpResponse(content_type='application/pdf')
+    response['Content-Disposition'] = 'attachment; filename="transactions.pdf"'
+
+    pdf = SimpleDocTemplate(response, pagesize=letter)
+    table = Table(data)
+    table.setStyle(TableStyle([
+        ('BACKGROUND', (0, 0), (-1, 0), colors.HexColor("#0095ff")),
+        ('TEXTCOLOR', (0, 0), (-1, 0), colors.white),
+        ('ALIGN', (0, 0), (-1, -1), 'CENTER'),
+        ('FONTNAME', (0, 0), (-1, 0), 'Helvetica-Bold'),
+        ('BOTTOMPADDING', (0, 0), (-1, 0), 12),
+        ('BACKGROUND', (0, 1), (-1, -1), colors.whitesmoke),
+        ('GRID', (0, 0), (-1, -1), 1, colors.black),
+    ]))
+    pdf.build([table])
+    return response
+
+def download_transactions(request):
+    file_type = request.GET.get('file_type')
+    if file_type == 'csv':
+        return download_transactions_csv(request)
+    elif file_type == 'pdf':
+        return download_transactions_pdf(request)
 
 @csrf_exempt
 @login_required
